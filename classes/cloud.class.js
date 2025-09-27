f61d6218a9b4e10bbb666d8b1a5e48709a68c0d2
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-09-27 15:30:05 +0200

    refactor: define default values (y, width, height) in Cloud as class properties

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,12 +1,12 @@
 class Cloud extends MovableObject {
+    y = 20;
+    width = 500;
+    height = 250;
 
     constructor(){
         super()
         this.loadImage('assets/img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500; // Zahl zwischen 200 und 700
-        this.y = 50;
-        this.width = 500;
-        this.height = 250;
     }
 }
