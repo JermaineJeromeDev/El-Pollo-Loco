1e1474b80a8682a2e5bdba0d00c746ba9320bc59
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-09-29 12:46:05 +0200

    Remove currentImage initialization from Character class

--- classes/character.class.js
+++ classes/character.class.js
@@ -11,7 +11,6 @@ class Character extends MovableObject{
             'assets/img/2_character_pepe/2_walk/W-25.png',
             'assets/img/2_character_pepe/2_walk/W-26.png'
         ];
-        currentImage = 0;
     
     constructor(){
         super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
