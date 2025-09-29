6656ab4c09158196574dfbbeb16840cc29fbde07
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-09-29 14:59:13 +0200

    Adjusted cloud animation: replaced setInterval with moveLeft() for leftward movement.

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,19 +2,17 @@ class Cloud extends MovableObject {
     y = 20;
     width = 500;
     height = 250;
+    
 
     constructor(){
         super().loadImage('assets/img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500; // Zahl zwischen 200 und 700
-
         this.animate();
     }
 
 
     animate() {
-        setInterval(() => {
-            this.x -= 0.15;
-        }, 1000 / 60);
+        this.moveLeft();
     }
 }
