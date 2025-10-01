7541329bc50c85b34ee31962d385654b69bc0590
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-01 16:03:58 +0200

    feat: add jumping animations and controls - Added `IMAGES_JUMPING` array for jump sprites - Update `animate()` to play jump animation when character is airborne - Apply upward speed when `UP` key is pressed - Walking animation still plays when moving on ground

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MovableObject{
 
-    y = 70;
+    y = 160;
     height = 280;
     width = 130;
     speed = 10;
@@ -12,14 +12,30 @@ class Character extends MovableObject{
         'assets/img/2_character_pepe/2_walk/W-25.png',
         'assets/img/2_character_pepe/2_walk/W-26.png'
     ];
+
+    IMAGES_JUMPING = [
+        'assets/img/2_character_pepe/3_jump/J-31.png',
+        'assets/img/2_character_pepe/3_jump/J-32.png',
+        'assets/img/2_character_pepe/3_jump/J-33.png',
+        'assets/img/2_character_pepe/3_jump/J-34.png',
+        'assets/img/2_character_pepe/3_jump/J-35.png',
+        'assets/img/2_character_pepe/3_jump/J-36.png',
+        'assets/img/2_character_pepe/3_jump/J-37.png',
+        'assets/img/2_character_pepe/3_jump/J-38.png',
+        'assets/img/2_character_pepe/3_jump/J-39.png'
+    ]
+
     world;
+    // walking_sound = new Audio();
     
     constructor(){
         super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
+
 
     animate(){
 
@@ -27,23 +43,35 @@ class Character extends MovableObject{
             if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                // this.walking_sound.play();
             }
 
             if(this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                // this.walking_sound.play();
             }
+
+            if(this.world.keyboard.UP) {
+                this.speedY = 20;
+            }
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval( () => {
 
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            if(this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
     }
     
+
     jump(){
 
     }
