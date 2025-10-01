39aed70c82cac81d49fa66cd16e4b4ff3870e624
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-01 15:25:15 +0200

    feat: add basic gravity system - Added `speedY` and `acceleration` properties - Implemented `applyGravity()` to update vertical position over time - Added `isAboveGround()` helper to limit falling - Prepares MovableObject for jump and falling mechanics

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,11 +8,30 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1.5;
+
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+
+    isAboveGround() {
+        return this.y < 150;
+    }
+
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
 
     /**
      * 
@@ -26,6 +45,7 @@ class MovableObject {
         });
     }
 
+
     playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = images[i];
@@ -33,9 +53,11 @@ class MovableObject {
                 this.currentImage ++;
     }
 
+
     moveRight() {
         console.log('Moving right');
     }
+
 
     moveLeft() {
         setInterval(() => {
