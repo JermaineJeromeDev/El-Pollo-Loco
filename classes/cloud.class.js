f56580862098cdb7c50b3f0fa7d6692190f56eae
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-02 11:19:53 +0200

    feat: allow Cloud to use random images and optional position

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,17 +2,24 @@ class Cloud extends MovableObject {
     y = 20;
     width = 500;
     height = 250;
-    
 
-    constructor(){
-        super().loadImage('assets/img/5_background/layers/4_clouds/1.png');
+    constructor(x = Math.random() * 500, y = 20) {
+        let images = [
+            'assets/img/5_background/layers/4_clouds/1.png',
+            'assets/img/5_background/layers/4_clouds/2.png'
+        ];
+        let randomIndex = Math.floor(Math.random() * images.length);
+        super().loadImage(images[randomIndex]);
 
-        this.x = Math.random() * 500; // Zahl zwischen 200 und 700
+        this.x = x;
+        this.y = y;
+
         this.animate();
     }
 
-
     animate() {
-        this.moveLeft();
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60); // 60 FPS
     }
 }
