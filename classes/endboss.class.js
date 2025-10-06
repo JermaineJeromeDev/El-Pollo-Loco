2dc90973cab686d9103b0a28bdea51e8410b1c4d
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-06 12:04:00 +0200

    Added hitbox offsets and visual debug frame to Endboss Class
    - Introduced offset object for more accurate collision boundaries
    - Implemented drawFrame() to visualize hitboxes (red outlines)
    - Maintained all movement and animation behavior unchanged

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -3,6 +3,12 @@ class Endboss extends MovableObject {
     height = 450;
     width = 300;
     y = 0;
+    offset = {
+        top: 90,
+        bottom: 40,
+        left: 24,
+        right: 20
+    }
 
     IMAGES_WALKING = [
         'assets/img/4_enemie_boss_chicken/2_alert/G5.png',
@@ -27,5 +33,19 @@ class Endboss extends MovableObject {
         setInterval( () => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
+    }
+
+    drawFrame(ctx){
+        super.drawFrame(ctx);
+        ctx.beginPath();
+        ctx.lineWidth = '2';
+        ctx.strokeStyle = 'red';
+        ctx.rect(
+            this.x + this.offset.left,
+            this.y + this.offset.top,
+            this.width - this.offset.left - this.offset.right,
+            this.height - this.offset.top - this.offset.bottom
+        );
+        ctx.stroke();
     }
 }
