cc14c58c5f81f95ff50b4ac5ee21d59bcb69962a
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-06 12:01:22 +0200

    Refactored World.draw() for cleaner structure and consistent formatting
    - Unified indentation and code style in draw() method
    - Removed redundant semicolons for readability
    - Maintained full functionality of rendering and collision checks

--- classes/world.class.js
+++ classes/world.class.js
@@ -35,17 +35,12 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
         this.ctx.translate(this.camera_x, 0);
-
-        this.addObjectsToMap(this.level.backgroundObjects);
-
+        this.addObjectsToMap(this.level.backgroundObjects)
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
-
         this.ctx.translate(-this.camera_x, 0);
-
         // Draw() wird immer wieder aufgerufen
         let self = this;
         requestAnimationFrame(function() {
