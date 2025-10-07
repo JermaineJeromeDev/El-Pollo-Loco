4d961523b579f19b8e523afd05df43e0a320e6a6
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-07 14:39:04 +0200

    Refactor: Move image/draw methods to DrawableObject - Moved all image loading and drawing functions from MovableObject to DrawableObject

---
+++ classes/drawable-object.class.js
@@ -0,0 +1,33 @@
+class DrawableObject {
+    img;
+    imageCache = {};
+    currentImage = 0;
+    x = 120;
+    y = 280;
+    width = 100;
+    height = 150;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr){
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+}
