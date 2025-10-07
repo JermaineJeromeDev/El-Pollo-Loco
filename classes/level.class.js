8429b3f8e3a60e3a9db646b9509ec79443ecbd2a
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-07 13:07:09 +0200

    feat: add coins array to Level class and update constructor

--- classes/level.class.js
+++ classes/level.class.js
@@ -2,11 +2,13 @@ class Level {
     clouds;
     enemies;
     backgroundObjects;
+    coins;
     level_end_x = 2260;
 
-    constructor(clouds, enemies, backgroundObjects) {
+    constructor(clouds, enemies, coins, backgroundObjects) {
         this.clouds = clouds;
         this.enemies = enemies;
+        this.coins = coins;
         this.backgroundObjects = backgroundObjects;
     }
 }
