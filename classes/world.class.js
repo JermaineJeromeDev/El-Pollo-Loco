eb7c9263bcbcb300617ea9b99a27fb127c433d4e
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-08 11:33:37 +0200

    feat: Implement bottle throw with cooldown
    - Added `canThrow` flag to prevent multiple bottles being thrown when holding the D key.
    - Updated `checkThrowObjects()` and `throwBottle()` methods to enforce 1-second cooldown between throws.
    - Ensured bottle inventory decreases correctly and status bar updates after each throw.
    - Refactored code for clarity and maintainability.

--- classes/world.class.js
+++ classes/world.class.js
@@ -9,7 +9,7 @@ class World {
     statusBarCoins = new StatusBarCoins();
     statusBarBottles = new StatusBarBottles();
     throwableObjects = [];
-
+    canThrow = true; 
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -21,52 +21,79 @@ class World {
         this.checkCollisions();
     }
 
-
     setWorld() {
         this.character.world = this;
     }
 
-
-    run() { 
+    run() {
         setInterval(() => {
             this.checkCollisions();
             this.checkThrowObjects();
+            this.checkCoinCollision();
+            this.checkBottleCollision();
         }, 50);
     }
 
-
+    // ----------------- Flaschen werfen -----------------
     checkThrowObjects() {
-        if(this.keyboard.D) {
-            let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100)
-            this.throwableObjects.push(bottle);
+        if(this.keyboard.D && this.character.bottle > 0 && this.canThrow) {
+            this.throwBottle();
         }
     }
 
+    throwBottle() {
+        let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+        this.throwableObjects.push(bottle);
+        this.character.bottle -= 20;
+        if(this.character.bottle < 0) this.character.bottle = 0;
+        this.statusBarBottles.setPercentageBottles(this.character.bottle);
+        this.canThrow = false;
+        setTimeout(() => {
+            this.canThrow = true;
+        }, 1000);
+    }
+
+    // ----------------- Kollisionen -----------------
     checkCollisions() {
         this.level.enemies.forEach((enemy) => {
-                if(enemy instanceof Chicken || enemy instanceof ChickenSmall || enemy instanceof Endboss) {
-                if (this.character.isColliding(enemy)) {
+            if(enemy instanceof Chicken || enemy instanceof ChickenSmall || enemy instanceof Endboss) {
+                if(this.character.isColliding(enemy)) {
                     this.character.hit();
-                    this.statusBarHealth.setPercentage(this.character.energy)
+                    this.statusBarHealth.setPercentage(this.character.energy);
                 }
             }
         });
     }
 
+    checkCoinCollision() {
+        this.level.coins.forEach((coin, index) => {
+            if(this.character.isColliding(coin)) {
+                this.character.addCoin();
+                this.statusBarCoins.setPercentageCoins(this.character.coin);
+                this.level.coins.splice(index, 1)
+            }
+        });
+    }
 
+    checkBottleCollision() {
+        this.level.bottles.forEach((bottle, index) => {
+            if(this.character.isColliding(bottle)) {
+                this.character.addBottle();
+                this.statusBarBottles.setPercentageBottles(this.character.bottle);
+                this.level.bottles.splice(index, 1)
+            }
+        });
+    }
+
+    // ----------------- Draw() -----------------
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.backgroundObjects);
 
         this.ctx.translate(-this.camera_x, 0);
-        // ------ Spaced for fixed objects ------ 
-        this.addObjectsToMap([this.statusBarHealth]);
-        this.addObjectsToMap([this.statusBarBottles]);
-        this.addObjectsToMap([this.statusBarCoins]);
-        this.ctx.translate(this.camera_x, 0); 
+        this.addObjectsToMap([this.statusBarHealth, this.statusBarBottles, this.statusBarCoins]);
+        this.ctx.translate(this.camera_x, 0);
 
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
@@ -77,29 +104,19 @@ class World {
 
         this.ctx.translate(-this.camera_x, 0);
 
-        // Draw() wird immer wieder aufgerufen
-        let self = this;
-        requestAnimationFrame(function() {
-            self.draw();
-        });
+        requestAnimationFrame(() => this.draw());
     }
-
 
     addObjectsToMap(objects){
-        objects.forEach(o => {
-            this.addToMap(o);
-        });
+        objects.forEach(o => this.addToMap(o));
     }
-
 
     addToMap(mo){
-        mo.otherDirection && this.flipImage(mo)
+        if(mo.otherDirection) this.flipImage(mo);
         mo.draw(this.ctx);
         mo.drawFrame(this.ctx);
-
-        mo.otherDirection && this.flipImageBack(mo)
+        if(mo.otherDirection) this.flipImageBack(mo);
     }
-
 
     flipImage(mo) {
         this.ctx.save();
@@ -108,7 +125,6 @@ class World {
         mo.x = mo.x * -1;
     }
 
-    
     flipImageBack(mo) {
         mo.x = mo.x * -1;
         this.ctx.restore();
