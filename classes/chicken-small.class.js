3741d1d14abcb05e4c4a9b42250952c3f36b7be1
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-08 16:57:22 +0200

    Refactor ChickenSmall: add death handling, stop movement and animation on death

--- classes/chicken-small.class.js
+++ classes/chicken-small.class.js
@@ -8,6 +8,11 @@ class ChickenSmall extends MovableObject {
         'assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
     ];
+
+    IMAGE_DEAD = [
+        'assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+
     offset = {
         top: 6,
         bottom: 6,
@@ -16,39 +21,56 @@ class ChickenSmall extends MovableObject {
     };
 
 
-
     constructor(){
         super().loadImage('assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImage(this.IMAGE_DEAD);
 
         this.x = 200 + Math.random() * 2000;
         this.speed = 0.15 + Math.random() * 0.8;
         this.animate();
     }
 
-    animate(){
-        setInterval(() => {
-            if(this.x > 0){
-                this.moveLeft();
-            }
+    animate() {
+    this.moveChicken();
+    this.checkIfDead(); 
+    }
+
+    moveChicken() {
+        this.moveInterval = setInterval(() => {
+            this.moveLeft();
         }, 1000 / 60);
 
-        setInterval( () => {
+        this.animationInterval = setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
     }
 
-    drawFrame(ctx){
-        super.drawFrame(ctx);
-        ctx.beginPath();
-        ctx.lineWidth = '2';
-        ctx.strokeStyle = 'red';
-        ctx.rect(
-            this.x + this.offset.left,
-            this.y + this.offset.top,
-            this.width - this.offset.left - this.offset.right,
-            this.height - this.offset.top - this.offset.bottom
-        );
-        ctx.stroke();
+    checkIfDead() {
+        this.deathCheckInterval = setInterval(() => {
+            if (this.isDead()) {
+                this.handleDeath();
+            }
+        }, 100);
+    }
+
+    handleDeath() {
+        if (this.isDeadHandled) return;
+        this.isDeadHandled = true;
+        this.loadImage(this.IMAGE_DEAD);
+        clearInterval(this.moveInterval);
+        clearInterval(this.animationInterval);
+        clearInterval(this.deathCheckInterval);
+        setTimeout(() => {
+            let fallSpeed = 2; 
+            let groundLevel = 365;
+
+            let fallInterval = setInterval(() => {
+                this.y += fallSpeed;
+                if (this.y >= groundLevel) {
+                    clearInterval(fallInterval);
+                }
+            }, 40);
+        }, 300);
     }
 }
