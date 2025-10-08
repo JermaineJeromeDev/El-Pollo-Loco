f962214960cd03788bfa91d82df8e432378ae073
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-08 10:01:10 +0200

    feat(level): add bottles to Level class

--- classes/level.class.js
+++ classes/level.class.js
@@ -2,13 +2,15 @@ class Level {
     clouds;
     enemies;
     coins;
+    bottles;
     backgroundObjects;
     level_end_x = 2260;
 
-    constructor(clouds, enemies, coins, backgroundObjects) {
+    constructor(clouds, enemies, coins, bottle, backgroundObjects) {
         this.clouds = clouds;
         this.enemies = enemies;
         this.coins = coins;
+        this.bottles = bottle;
         this.backgroundObjects = backgroundObjects;
     }
 }
