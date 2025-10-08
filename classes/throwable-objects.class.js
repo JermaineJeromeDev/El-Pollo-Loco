2cf7e6bcfcd4650c058cf55e0e4bdc14b77c8b82
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-08 13:51:12 +0200

    Implement throwable bottle with rotation, directional movement, and splash animation on impact

--- classes/throwable-objects.class.js
+++ classes/throwable-objects.class.js
@@ -1,20 +1,88 @@
 class ThrowableObject extends MovableObject {
-    constructor(x, y) {
+    throwBottleAir = false;
+    bottleSplash = false;
+    isBreaking = false;
+    offset = {
+        left: 20,
+        top: 20,
+        right: 20,
+        bottom: 20
+    };
+    intervalId;
+
+    IMAGES_ROTATE = [
+        'assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+
+    IMAGES_BOTTLE_SPLASH = [
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ];
+
+    constructor(x, y, direction) {
         super();
+        this.otherDirection = direction;
         this.loadImage('assets/img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATE);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
         this.x = x;
         this.y = y;
         this.height = 70;
         this.width = 60;
         this.throw();
+        this.animate();
+    }
+    
+    throw() {
+        this.throwBottleAir = true;
+        this.speedY = 20;
+        if (this.otherDirection == true) {
+        this.speedX = -10;  
+        } else {
+        this.speedX = 10;  
     }
 
+        this.applyGravityAndMove();
+    }
 
-    throw() {
-        this.speedY = 20;
-        this.applyGravity();
+    applyGravityAndMove() {
+        this.intervalId = setInterval(() => {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+            this.x += this.speedX;
+
+            if (this.y >= 380) {
+                this.y = 380;
+                this.breakAndSplash();
+            }
+        }, 1000 / 60);
+    }
+
+    breakAndSplash() {
+        if (!this.isBreaking) {
+            this.throwBottleAir = false;
+            this.isBreaking = true;
+            this.speedX = 0;
+            this.speedY = 0;
+            clearInterval(this.intervalId);
+        }
+        this.playAnimation(this.IMAGES_BOTTLE_SPLASH, () => {
+            this.fadeOut();
+        });
+    }
+
+    animate() {
         setInterval(() => {
-            this.x += 10;
-        }, 25)
+            if (this.throwBottleAir) {
+                this.playAnimation(this.IMAGES_ROTATE);
+            }
+        }, 2000 / 60);
     }
 }
