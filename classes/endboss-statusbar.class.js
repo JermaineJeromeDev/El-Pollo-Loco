2ca4b9ef86926458685908b73d6f85f29d616d6f
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-10-09 13:28:15 +0200

    Add StatusBarEndboss class to manage end boss health display

---
+++ classes/endboss-statusbar.class.js
@@ -0,0 +1,46 @@
+class StatusBarEndboss extends DrawableObject {
+    percentage = 100;
+    IMAGES = [
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue0.png',
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue20.png',
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue40.png',
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue60.png',
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue80.png',
+        'assets/img/7_statusbars/2_statusbar_endboss/blue/blue100.png'
+    ];
+
+
+    constructor() {
+        super();
+        this.loadImages(this.IMAGES);
+        this.x = 520;
+        this.y = 0;
+        this.width = 200;
+        this.height = 60;
+        this.setPercentage(100);
+    }
+
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+
+    resolveImageIndex() {
+        if(this.percentage > 80) {
+            return 5;
+        } else if(this.percentage > 60) {
+            return 4;
+        } else if(this.percentage > 40) {
+            return 3;
+        } else if(this.percentage > 20) {
+            return 2;
+        } else if(this.percentage > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
