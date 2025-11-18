b019db4f53e2fc3f86c3182065bf545be6911edc
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-11-18 10:21:23 +0100

    Add JSDoc comments to Keyboard class for improved documentation

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -1,9 +1,26 @@
+/**
+ * Repräsentiert den Keyboard-Status
+ * @class Keyboard
+ */
 class Keyboard {
+    /** Linke Pfeiltaste */
     LEFT = false;
+    
+    /** Rechte Pfeiltaste */
     RIGHT = false;
+    
+    /** Obere Pfeiltaste */
     UP = false;
-    DOWN = false
+    
+    /** Untere Pfeiltaste */
+    DOWN = false;
+    
+    /** Leertaste (Springen) */
     SPACE = false;
+    
+    /** D-Taste (Werfen) */
     D = false;
+    
+    /** P-Taste */
     P = false;
 }
